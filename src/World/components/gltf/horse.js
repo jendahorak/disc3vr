aa04d2e6f233feff00b7b172c3f6167acca25875
jenda_horak <484526@muni.cz>
2023-11-05 10:20:42 +0100

    add

--- src/World/components/gltf/horse.js
+++ src/World/components/gltf/horse.js
@@ -5,14 +5,14 @@ import { setupModel } from './setupModel.js';
 async function loadHorse() {
   const loader = new GLTFLoader();
 
-  const horseData = await loader.loadAsync('models/Horse.glb');
+  const horseData = await loader.loadAsync('models/Horse_scaled_1m.glb');
 
   // console.log('Horse is here!', horseData);
 
   const horse = setupModel(horseData);
 
   // TODO - why is it scaling so wierd, models are too big.
-  horse.scale.set(0.01, 0.01, 0.01);
+  // horse.scale.set(0.01, 0.01, 0.01);
 
   console.log(horse);
   horse.position.set(0, 0, 0);
